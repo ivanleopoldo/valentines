c51f05e8dfb9412fb01c26a89cc8bac7ba7917b5
ivanleopoldo <ivanleopoldoc@gmail.com>
2026-02-13 18:55:58 +0800

    style: adds shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
     <div className="p-4 flex items-center justify-center h-screen">
       <p className="font-bold underline">Hello World</p>
+      <Button>Click Me</Button>
     </div>
   );
 }

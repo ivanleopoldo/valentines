47747bb179f6369381527d8ec0f4b3948926ba63
ivanleopoldo <ivanleopoldoc@gmail.com>
2026-02-13 18:15:36 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="p-4 flex items-center justify-center h-screen">
+      <p className="font-bold underline">Hello World</p>
+    </div>
+  );
+}
+
+export default App;
